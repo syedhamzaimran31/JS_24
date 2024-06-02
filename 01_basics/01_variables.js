8d31ea0ffa03ab99bf1c46bdcc57c689aa37b300
syedhamzaimran31 <syedhamzaimran31@gmail.com>
2024-06-02 16:45:21 +0500

    data types revision in javaScript

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+var name="Hamza123"
+let age="123"
+const isMale=true
+
+console.log(name, age, isMale)
+console.table([name, age, isMale])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict"; //Treat everything as JS new version but it is not necessary
+// alert("Please install") //Not allowed because we are using node js not browser
+
+let myName="Syed Project";  //String
+let myAge=24;               //number
+let myGender=true;          //boolean
+let variableName=null;      //Object
+let variableNotDefined;     //undefined
+let bigInteger=111111111111111111111111111111111111111111n;
+
+console.table([ typeof myName,typeof myAge,typeof myGender,typeof variableName,typeof variableNotDefined,typeof bigInteger]);
